89ffc1625d7a758fe99b71a2253d47fe8ab3c551
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2026-04-06 23:15:28 -0600

    Code Challenge 3

---
+++ Module_1/CC_3.js
@@ -0,0 +1,51 @@
+function Image(title, artist, date) {
+    this.title = title;
+    this.artist = artist;
+    this.date = date;
+}
+
+function getImage(title, artist, date) {
+    return{
+        title,
+        artist,
+        date
+    }
+}
+
+let images = {
+    list: [],
+    contains: function(title) {
+        return this.list.some(image => image.title === title);
+    },
+    add: function(title, artist, date) {
+        if(this.contains(title)) {
+            return false;
+        }
+
+        this.list.push(new Image(title, artist, date)); 
+        return true;
+    },
+    show: function() {
+        if(this.list.length == 0) {
+            console.log('List Empty')
+            return;
+        }
+
+        this.list.forEach(image => console.log(`${image.title}, ${image.artist}, ${image.date}`))
+    },
+    clear: function() {
+        if(this.list.length == 0) {
+            return;
+        }
+
+        this.list.length = 0;
+    }
+}
+
+images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
+images.add('The Last Supper', 'Leonardo da Vinci', 1495);
+images.add('The Starry Night', 'Vincent van Gogh', 1889);
+images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
+images.show();
+images.clear();
+images.show();
